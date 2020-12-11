3afff5c4787250170f85d14c43487c598df7aaa7
Mikalai Hurynovich <gurinovich-ns@mail.ru>
2020-12-11 12:28:58 +0300

    add: Promise solution

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -28,8 +28,12 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(answer) {
+  return new Promise((resolve, reject) => {
+    if (typeof answer !== 'boolean') reject(new Error('Wrong parameter is passed! Ask her again.'));
+    if (answer) resolve('Hooray!!! She said "Yes"!');
+    else if (!answer) resolve('Oh no, she said "No".');
+  });
 }
 
 
@@ -48,8 +52,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(arr) {
+  return Promise.all(arr);
 }
 
 /**
@@ -71,8 +75,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(arr) {
+  return Promise.race(arr);
 }
 
 /**
